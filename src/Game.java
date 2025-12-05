2b0a2412bc61a4aed5728fd519c5cf4517b42c53
fatma <fatmatnt4@gmail.com>
2025-12-05 17:30:12 +0200

    init commit

---
+++ src/Game.java
@@ -0,0 +1,4 @@
+import javax.swing.*;
+
+public class Game extends JFrame {
+}
---
+++ src/GameGLListener.java
@@ -0,0 +1,5 @@
+import javax.media.opengl.GLEventListener;
+import java.awt.event.KeyListener;
+
+public class GameGLListener implements GLEventListener, KeyListener {
+}
