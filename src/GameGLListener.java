b6afb58a423a2261575771c2aeb8146b1d4b665c
fatma <fatmatnt4@gmail.com>
2025-12-08 23:07:31 +0200

    record a goal and draw ball

--- src/GameGLListener.java
+++ src/GameGLListener.java
@@ -1,36 +1,49 @@
+import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLEventListener;
+import javax.media.opengl.glu.GLU;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.IOException;
 
 public class GameGLListener implements GLEventListener, KeyListener {
-    private double xBall=0;
-    private double yBall=0;
-    private double dxBall=0.02;
-    private double dyBall=0.01;
-    private double xmax=350;
-    private double xmin=-350;
-    private double ymax=350;
-    private double ymin=-350;
+
+
+    private double xBall=MAX_X/2;
+    private double yBall=MAX_Y/2;
+    private double dxBall= 10 ; ;
+    private double dyBall= 8;;
+
+
+
     private int score1=0;
     private int score2=0;
-    public void updateball(){
+    String textureName = "B1.png";
+    TextureReader.Texture texture;
+    int ballTexture[] = new int[1];
+
+    public void updateBall(){
         xBall += dxBall;
         yBall += dyBall;
         //تغيير الاتجاه لو الكرة خبطت فى الحواف
-        if(xBall >= xmax ||xBall <= xmin){
+
+        if(xBall+20 >= MAX_X || xBall <= MIN_X){
             dxBall = -dxBall;
         }
-        if(yBall >= ymax ||yBall <= ymin){
+        if(yBall+20>= MAX_Y || yBall <= MIN_Y){
             dyBall = -dyBall;
         }
+        // collision with players
+
     }
 
     public void player1MakeGoal(){
         score1++;
+        System.out.println("player1 Make Goal");
     }
     public void player2MakeGoal(){
         score2++;
+        System.out.println("player2 Make Goal");
     }
     public boolean hasWinner(){
         if(score1==3)
@@ -49,9 +62,12 @@ public class GameGLListener implements GLEventListener, KeyListener {
             return null;
     }
     public void reset(){
-        xBall=0;
-        yBall=0;
+        xBall=MAX_X/2;
+        yBall=MAX_Y/2;
+
+        dxBall=-dxBall;
     }
+
 
     @Override
     public void keyTyped(KeyEvent e) {
@@ -70,22 +86,61 @@ public class GameGLListener implements GLEventListener, KeyListener {
 
     @Override
     public void init(GLAutoDrawable glAutoDrawable) {
+        GL gl = glAutoDrawable.getGL();
+
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        gl.glMatrixMode(GL.GL_PROJECTION);
+        gl.glLoadIdentity();
+        gl.glOrtho(MIN_X, MAX_X, MIN_Y, MAX_Y, -1.0, 1.0);
+
+        gl.glMatrixMode(GL.GL_MODELVIEW);
+        gl.glLoadIdentity();
+        gl.glEnable(GL.GL_TEXTURE_2D);
+        gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
+
+
+        gl.glGenTextures(1, ballTexture, 0);
+
+        try {
+            texture = TextureReader.readTexture("assets//" + textureName , true);
+            gl.glBindTexture(GL.GL_TEXTURE_2D, ballTexture[0]);
+
+
+            new GLU().gluBuild2DMipmaps(
+                    GL.GL_TEXTURE_2D,
+                    GL.GL_RGBA,
+                    texture.getWidth(), texture.getHeight(),
+                    GL.GL_RGBA,
+                    GL.GL_UNSIGNED_BYTE,
+                    texture.getPixels()
+            );
+        } catch( IOException e ) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void display(GLAutoDrawable glAutoDrawable) {
 
+        GL gl=glAutoDrawable.getGL();
 
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+        gl.glLoadIdentity();
+
+        drawBall(gl,xBall,yBall,30);
         //update ball position
-        updateball();
+        updateBall();
+
+
         // make goal
-        if(xBall<=xmin){
-            player1MakeGoal();
+        if(xBall+30 <=50 && yBall +30 >=150 && yBall +30 <= 300){
+            player2MakeGoal();
             reset();
         }
-        if(xBall>=xmax){
-            player2MakeGoal();
+        if(xBall+30>=750 && yBall +30 >=150 && yBall +30 <= 300){
+            player1MakeGoal();
             reset();
         }
         // win massage
@@ -102,6 +157,35 @@ public class GameGLListener implements GLEventListener, KeyListener {
 
     @Override
     public void displayChanged(GLAutoDrawable glAutoDrawable, boolean b, boolean b1) {
+
+    }
+    public void drawBall(GL gl,double xBall,double yBall,double size){
+
+
+
+        gl.glEnable(GL.GL_BLEND);
+        gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
+
+
+        gl.glBindTexture(GL.GL_TEXTURE_2D, ballTexture[0]);
+
+
+
+        gl.glBegin(GL.GL_QUADS);
+
+        gl.glTexCoord2f(0.0f, 0.0f);
+        gl.glVertex3d(xBall, yBall, 0.0);
+        gl.glTexCoord2f(1.0f, 0.0f);
+        gl.glVertex3d(xBall+size, yBall, 0.0);
+        gl.glTexCoord2f(1.0f, 1.0f);
+        gl.glVertex3d(xBall+size, yBall+size, 0.0);
+        gl.glTexCoord2f(0.0f, 1.0f);
+        gl.glVertex3d(xBall, yBall+size, 0.0);
+        gl.glEnd();
+
+
+
+        gl.glDisable(GL.GL_BLEND);
 
     }
 }
