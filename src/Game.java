4c6a62ce9ef6ea42e083a2c65741120c285b6bf5
Aya Hassan <haya35633@gmail.com>
2025-12-09 21:43:35 +0200

    StartWindow

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,99 @@
-import javax.swing.*;
+import com.sun.opengl.util.Animator;
+import com.sun.opengl.util.FPSAnimator;
 
-public class Game extends JFrame {
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLCanvas;
+import javax.media.opengl.GLCapabilities;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.FileInputStream;
+
+public class Game<AudioStream> extends JFrame implements ActionListener , MouseListener  {
+    private Animator animator;
+    private GLCanvas glcanvas;
+    private GameListener listener = new GameGLListener();
+    private AirTableGLListener AirTable = new AirTableGLListener();
+    JPanel jPanel = new JPanel();
+    JTextField UserName = new JTextField("");
+    JLabel  UsernameTitle = new JLabel("                                Player Name : " );
+    JLabel space = new JLabel("                                                        ");
+    boolean onePlayer = false ;
+    boolean TwoPlayer = false ;
+
+
+    public static void main(String[] args) {
+        new Game().animator.start();
+        // ابدأ بالقائمة
+    }
+
+    public Game() {
+        super("The Air Hockey Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        glcanvas = new GLCanvas();
+        glcanvas.addGLEventListener(listener);
+        showStartWindow();
+        glcanvas.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (e.getX() < 454 && e.getX() > 328 && e.getY() < 423 && e.getY() > 387) {
+                    System.out.println("Switching to Game...");
+                    new AirTable().setVisible(true);
+                }
+            }
+        });
+        animator = new FPSAnimator(10);
+        animator.add(glcanvas);
+
+        getContentPane().add(glcanvas, BorderLayout.CENTER);
+        setSize(800, 500);
+        setLocationRelativeTo(this);
+        setVisible(true);
+        setFocusable(true);
+        glcanvas.requestFocus();
+    }
+
+   private void showStartWindow() {
+       jPanel.setLayout(new GridLayout(1 , 3, 50, 50));
+       jPanel.add(UsernameTitle);
+       jPanel.add(UserName);
+       jPanel.add(space);
+       this.add(jPanel, BorderLayout.NORTH);
+       glcanvas.addMouseListener(new GameGLListener());
+       glcanvas.addMouseListener(this);
+       glcanvas.addMouseListener(new AirTableGLListener());
+   }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
+
+
--- src/GameGLListener.java
+++ src/GameGLListener.java
@@ -1,32 +1,118 @@
+import Texture.TextureReader;
+
+import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
-import javax.media.opengl.GLEventListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import javax.media.opengl.GLCanvas;
+import javax.media.opengl.glu.GLU;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
 
-public class GameGLListener implements GLEventListener, KeyListener {
-    @Override
-    public void keyTyped(KeyEvent e) {
+import static java.lang.System.in;
+import static java.lang.System.setOut;
 
-    }
+public class GameGLListener extends GameListener implements  MouseListener {
+    String textureNames[] = {"air hockey start window.png"};
+    TextureReader.Texture texture[] = new TextureReader.Texture[textureNames.length];
+    int textures[] = new int[textureNames.length];
+    GL gl;
+    GLAutoDrawable glAutoDrawable;
+    static Clip clip;
+    File f = new File("D:\\New folder\\project33\\.idea\\Sound\\Air hockey sound.wav");
 
-    @Override
-    public void keyPressed(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-
-    }
 
     @Override
     public void init(GLAutoDrawable glAutoDrawable) {
+        GL gl = glAutoDrawable.getGL();
+        gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);  //This Will Clear The Background Color To Black
+        gl.glEnable(GL.GL_TEXTURE_2D);  // Enable Texture Mapping
+        gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
+        onSound();
+        //number of textures,array to hold the indeces
+        gl.glGenTextures(textureNames.length, textures, 0);
+        for (int i = 0; i < textureNames.length; i++) {
+            try {
+                texture[i] = TextureReader.readTexture(assetsFolderName + "//" + textureNames[i], true);
+                gl.glBindTexture(GL.GL_TEXTURE_2D, textures[i]);
 
+//              mipmapsFromPNG(gl, new GLU(), texture[i]);
+                new GLU().gluBuild2DMipmaps(
+                        GL.GL_TEXTURE_2D,
+                        GL.GL_RGBA, // Internal Texel Format,
+                        texture[i].getWidth(), texture[i].getHeight(),
+                        GL.GL_RGBA, // External format from image,
+                        GL.GL_UNSIGNED_BYTE,
+                        texture[i].getPixels() // Imagedata
+                );
+            } catch (IOException e) {
+                System.out.println(e);
+                e.printStackTrace();
+            }
+        }
+    }
+    int X =0  , Y= 0 ;
+    GLCanvas glCanvas = new GLCanvas();
+    AirTableGLListener AirTable = new AirTableGLListener();
+    public void display(GLAutoDrawable glAutoDrawable) {
+        GL gl = glAutoDrawable.getGL();
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT);  //Clear The Screen And The Depth Buffer
+        gl.glLoadIdentity();
+        DrawBackground(gl);
+//          DrawGraph(gl);
+//          DrawSprite(gl);
     }
 
-    @Override
-    public void display(GLAutoDrawable glAutoDrawable) {
+    public void DrawBackground(GL gl) {
+        gl.glEnable(GL.GL_BLEND);
+        gl.glBindTexture(GL.GL_TEXTURE_2D, textures[0]);    // Turn Blending On
 
+//        gl.glColor3f(0, 0.5f, 0.5f);
+        gl.glPushMatrix();
+        gl.glBegin(GL.GL_QUADS);
+        // Front Face
+        gl.glTexCoord2f(0.0f, 0.0f);
+        gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+        gl.glTexCoord2f(1.0f, 0.0f);
+        gl.glVertex3f(1.0f, -1.0f, -1.0f);
+        gl.glTexCoord2f(1.0f, 1.0f);
+        gl.glVertex3f(1.0f, 1.0f, -1.0f);
+        gl.glTexCoord2f(0.0f, 1.0f);
+        gl.glVertex3f(-1.0f, 1.0f, -1.0f);
+        gl.glEnd();
+        gl.glPopMatrix();
+
+        gl.glDisable(GL.GL_BLEND);
+    }
+
+    public void DrawSprite(GL gl) {
+        gl.glEnable(GL.GL_BLEND);
+        gl.glBindTexture(GL.GL_TEXTURE_2D, textures[0]);    // Turn Blending On
+
+//      gl.glColor3f(0, 0, 0);
+        gl.glPushMatrix();
+//      gl.glTranslated( 0, 0, 0);
+        gl.glScaled(0.5, 0.5, 1);
+        //System.out.println(x +" " + y);
+        gl.glBegin(GL.GL_QUADS);
+        // Front Face
+        gl.glTexCoord2f(0.0f, 0.0f);
+        gl.glVertex3f(-1.0f, -0.5f, -1.0f);
+        gl.glTexCoord2f(1.0f, 0.0f);
+        gl.glVertex3f(1.0f, -0.5f, -1.0f);
+        gl.glTexCoord2f(1.0f, 1.0f);
+        gl.glVertex3f(1.0f, 1.0f, -1.0f);
+        gl.glTexCoord2f(0.0f, 1.0f);
+        gl.glVertex3f(-1.0f, 1.0f, -1.0f);
+        gl.glEnd();
+        gl.glPopMatrix();
+
+        gl.glDisable(GL.GL_BLEND);
     }
 
     @Override
@@ -38,4 +124,47 @@ public class GameGLListener implements GLEventListener, KeyListener {
     public void displayChanged(GLAutoDrawable glAutoDrawable, boolean b, boolean b1) {
 
     }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    void onSound() {
+        try {
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(f.toURI().toURL());
+            clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.loop(Clip.LOOP_CONTINUOUSLY); // أمر التكرار
+            clip.start();
+        } catch (Exception exception) {
+            System.out.println(exception.toString());
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if (e.getX() > 7 && e.getX() < 51 && e.getY() > 9 && e.getY() < 52) {
+            onSound();
+        } else if (e.getX() > 65 && e.getX() < 105 && e.getY() > 9 && e.getY() < 52) {
+            clip.stop();
+            clip.close();
+        }
+    }
+
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
 }
